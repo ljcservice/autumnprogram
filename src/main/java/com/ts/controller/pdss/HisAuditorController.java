79bef68946b576a06a11822255820319694e5a97
silong.xing <silong.xing@HLJ-xingsilong.ebaonet.corp>
2017-02-18 22:36:38 +0800

    合并

--- src/main/java/com/ts/controller/pdss/HisAuditorController.java
+++ src/main/java/com/ts/controller/pdss/HisAuditorController.java
@@ -1,5 +1,10 @@
 package com.ts.controller.pdss;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -42,7 +47,7 @@ import com.ts.entity.pdss.peaas.Beans.TOperationType;
 import com.ts.entity.pdss.peaas.Beans.TPrescPatMasterBean;
 import com.ts.entity.pdss.peaas.RSBeans.TPrescCheckRslt;
 import com.ts.entity.pdss.peaas.RSBeans.TPrescSecurityRslt;
-import com.ts.service.IHisGetPatientOrders;
+import com.ts.service.pdss.IHisGetPatientOrders;
 import com.ts.service.pdss.ias.manager.IAntiDrugAuditor;
 import com.ts.service.pdss.ias.manager.IAntiDrugSecurityChecker;
 import com.ts.service.pdss.pdss.Utils.CommonUtils;
@@ -857,18 +862,108 @@ public class HisAuditorController extends BaseController {
     /* 相互作用检查 */
 	@RequestMapping(value="/patVsVisitSigns")
 	@ResponseBody
-    public TDrugSecurityRslt drugInteractionCheck(TPatientOrder po)
+    public String drugInteractionCheck(String param)
     {
-        return drugsecuity.DrugInteractionCheck(po);
-        
-//		JSONObject o = JSONObject.fromObject(param);
-//		String patId = (String) o.get("patId");
-//		String visitId = (String) o.get("visitId");
-//		TPatVitalSigns[] t = antiDrugscr.getpatVsVisitSigns(patId, visitId);
-//		return t==null?null:JSONObject.fromObject(t).toString();
+		TDrugSecurityRslt t =null;
+		try {
+			TPatOrderDrug[]	pods = (TPatOrderDrug[])getObject(param,TPatOrderDrug.class,1);
+			t = drugsecuity.DrugInteractionCheck( pods);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return t==null?null:JSONObject.fromObject(t).toString();
 	}
     
-    public TDrugSecurityRslt DrugInteractionCheckS(String[] Drugs)
+	/**
+	 * 
+	 * @param param
+	 * @param class1
+	 * @param type 0返回类型为单个本体，1为返回数组
+	 * @return
+	 * @throws Exception
+	 */
+    private Object getObject(String param, Class class1,int type) throws Exception {
+    	Field[] fs =class1.getDeclaredFields();
+    	if(type==0){
+    		Object result = class1.newInstance();
+    		JSONObject obj = JSONObject.fromObject(param);
+    		for(Object o : obj.keySet()){
+    			for(Field f:fs){
+    				Class<?> clsType = f.getType();
+    				String name = f.getName();
+    				if(name.equals((String)o)){
+    					String strSet = "set" + name.substring(0, 1).toUpperCase() + name.substring(1, name.length());
+    					Method methodSet = class1.getDeclaredMethod(strSet,	clsType);
+						Object objValue = typeConversion(clsType,obj.get(o).toString());
+						methodSet.invoke(result, objValue);
+    				}
+    			}
+    		}
+    		return result;
+    	}else if(type==1){
+    		List<Object> s = new ArrayList<Object>();
+    		JSONArray arrys = JSONArray.fromObject(param);
+			for(int i=0;i<arrys.size();i++){
+				JSONObject obj= (JSONObject) arrys.get(i);
+				Object result = class1.newInstance();
+	    		for(Object o : obj.keySet()){
+	    			for(Field f:fs){
+	    				String name = f.getName();
+	    				if(name.equals((String)o)){
+	    					Class<?> clsType = f.getType();
+	    					String strSet = "set" + name.substring(0, 1).toUpperCase() + name.substring(1, name.length());
+	    					Method methodSet = class1.getDeclaredMethod(strSet,	clsType);
+							Object objValue = typeConversion(clsType,obj.get(o).toString());
+							methodSet.invoke(result, objValue);
+							break;
+	    				}
+	    			}
+	    		}
+	    		s.add(result) ;
+    		}
+
+    		return s.toArray();
+    	}
+		return null;
+	}
+
+    /**
+     * 得到类型值
+     */
+	public static Object typeConversion(Class<?> cls, String str) {
+	    Object obj = null;
+	    if("".equals(str)||str==null)
+	    	return null;
+	    String nameType = cls.getSimpleName();
+	    if ("Integer".equals(nameType)) {
+	        obj = Integer.valueOf(str);
+	    }else if ("String".equals(nameType)) {
+	        obj = str;
+	    }else if ("Float".equals(nameType)) {
+	        obj = Float.valueOf(str);
+	    }else if ("Double".equals(nameType)) {
+	        obj = Double.valueOf(str);
+	    }else if ("Boolean".equals(nameType)) {
+	        obj = Boolean.valueOf(str.equals("1")?true:false);
+	    }else if ("Long".equals(nameType)) {
+	        obj = Long.valueOf(str);
+	    }else if ("Short".equals(nameType)) {
+	        obj = Short.valueOf(str);
+	    }else if ("Character".equals(nameType)) {
+	        obj = str.charAt(1);
+	    }else if ("Date".equals(nameType)) {
+	        try {
+				obj = new SimpleDateFormat("yyyy-MM-dd").parse(str) ;
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
+	
+	    return obj;
+	}
+
+	public TDrugSecurityRslt DrugInteractionCheckS(String[] Drugs)
     {
         return drugsecuity.DrugInteractionCheckS(Drugs);
     }
@@ -1178,7 +1273,7 @@ public class HisAuditorController extends BaseController {
             String[][] diagnosisInfo, String[][] sensitiveInfo,String[][] patSigns,String[] patOperation)
     {
     	TPatientOrder      po = CommonUtils.getPatientOrder(doctorInfo, patientInfo, drugInfo, diagnosisInfo, sensitiveInfo, patSigns,patOperation);
-    	TDrugSecurityRslt dsr = drugsecuity.DrugInteractionCheck(po);
+    	TDrugSecurityRslt dsr = drugsecuity.DrugInteractionCheck(po.getPatOrderDrugs());
     	this.patientSavaBean.savePatientCheckInfo(po, dsr);
     	this.patientSavaBean.saveDrugInteractionCheckInfo(dsr);
     	return dsr;
@@ -1236,9 +1331,21 @@ public class HisAuditorController extends BaseController {
 			System.out.println(j.toString());
 			
 			TPatVitalSigns p1 = new TPatVitalSigns();
+			p1.setPatid("1111");
+			p1.setBloodLow("1");
 			TPatVitalSigns p2 = new TPatVitalSigns();
+			p2.setPatid("2222");
+			p2.setBloodLow("222222");
 			TPatVitalSigns[] dd =new TPatVitalSigns[]{p1,p2};
-			JSONArray w=JSONArray.fromObject("");
+			JSONArray w=JSONArray.fromObject(dd);
 			System.out.println(w.toString());
+			
+			try {
+				new HisAuditorController().getObject(w.toString(), TPatVitalSigns.class, 1);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 	}
 }
