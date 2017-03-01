93f7308570667349980572f1e0554519ef49830e
silong.xing <silong.xing@HLJ-xingsilong.ebaonet.corp>
2017-03-01 23:44:22 +0800

    缓存模板

--- src/main/java/com/ts/service/cache/CacheTemplate.java
+++ src/main/java/com/ts/service/cache/CacheTemplate.java
@@ -1,5 +1,9 @@
 package com.ts.service.cache;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.apache.commons.lang.time.StopWatch;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,11 @@ import com.ts.dao.redis.RedisDao;
 import com.ts.util.Tools;
 
 
+/**
+ * 
+ * @author silong.xing
+ *
+ */
 @Component
 public class CacheTemplate {
     @Autowired
@@ -16,43 +25,33 @@ public class CacheTemplate {
     
 	private static final Logger log = Logger.getLogger(CacheTemplate.class);
 
+	public <T> T cache(String keyV, CacheProcessor<T> proccessor) throws Exception {
+		return this.cache(keyV, null,true, 1 * 24 * 60 * 60, proccessor);
+	}
+	public <T> T cache(String keyV,int seconds, CacheProcessor<T> proccessor) throws Exception {
+		return this.cache(keyV, null,true, seconds, proccessor);
+	}
+	public <T> T cache(String keyV,boolean useCache, CacheProcessor<T> proccessor) throws Exception {
+		return this.cache(keyV,null,useCache,  1 * 24 * 60 * 60, proccessor);
+	}
 	public <T> T cache(String keyV, boolean useCache,int seconds, CacheProcessor<T> proccessor) throws Exception {
-		return this.cache(keyV, useCache,null, seconds, proccessor);
+		return this.cache(keyV,null, useCache, seconds, proccessor);
+	}
+	
+	public <T> T cache(String keyV, String keyAppend ,int seconds,CacheProcessor<T> proccessor) throws Exception {
+		return this.cache(keyV,keyAppend,true,  seconds, proccessor);
+	}
+	public <T> T cache(String keyV, String keyAppend ,CacheProcessor<T> proccessor) throws Exception {
+		return this.cache(keyV,keyAppend,true,  1 * 24 * 60 * 60, proccessor);
+	}
+	public <T> T cache(String keyV, String keyAppend,boolean useCache, CacheProcessor<T> proccessor) throws Exception {
+		return this.cache(keyV,keyAppend,useCache,  1 * 24 * 60 * 60, proccessor);
 	}
 
-	public <T> T cache(String keyV, boolean useCache,String keyAppend, CacheProcessor<T> proccessor) throws Exception {
-		return this.cache(keyV,useCache, keyAppend, 1 * 24 * 60 * 60, proccessor);
-	}
-	
-	/**
-	 * 默认使用缓存
-	 * @param keyV
-	 * @param proccessor
-	 * @return
-	 * @throws Exception 
-	 */
-	public <T> T cache(String keyV, CacheProcessor<T> proccessor) throws Exception {
-		return this.cache(keyV,true, null, 1 * 24 * 60 * 60, proccessor);
-	}
-	/**
-	 * 默认使用缓存
-	 * @param keyV
-	 * @param keyAppend
-	 * @param proccessor
-	 * @return
-	 * @throws Exception 
-	 */
-	public <T> T cache(String keyV, String keyAppend ,CacheProcessor<T> proccessor) throws Exception {
-		return this.cache(keyV,true, keyAppend, 1 * 24 * 60 * 60, proccessor);
-	}
-	
-	public <T> T cache(String keyV,boolean useCache, CacheProcessor<T> proccessor) throws Exception {
-		return this.cache(keyV,useCache, null, 1 * 24 * 60 * 60, proccessor);
-	}
 
 	@SuppressWarnings("unchecked")
-	public <T> T cache(String key,boolean useCache, String keyAppend, int seconds, CacheProcessor<T> proccessor) throws Exception {
-		String keyName = "KEY: # " + key + (keyAppend == null ? "" : ("," + keyAppend)) + " #";
+	public <T> T cache(String key,String keyAppend,boolean useCache,  int seconds, CacheProcessor<T> proccessor) throws Exception {
+		String keyName = "KEY:# " + key + (keyAppend == null ? "" : ("," + keyAppend)) + " #";
 		boolean cacheFrt = false;
 		if( useCache ){
 			log.debug(keyName + ",start");
@@ -93,4 +92,30 @@ public class CacheTemplate {
 		return ret;
 	}
 
+	/**
+	 * 存放数据，存放什么类型取出时强转什么类型
+	 * @param key	存储的key
+	 * @param seconds 单位为秒
+	 * @param obj	存储的值
+	 * @return
+	 */
+	public String setObject(String key, String keyAppend, int seconds, Object obj) {
+		String keyName = "KEY:# " + key + (keyAppend == null ? "" : ("," + keyAppend)) + " #";
+		return redisDao.setObject(keyName, seconds, obj);
+	}
+	public String setObject(String key, int seconds, Object obj) {
+		return redisDao.setObject(key, seconds, obj);
+	}
+	/**
+	 * 从缓存获取数据，存放什么类型取出时强转什么类型
+	 * @param key
+	 * @return Object
+	 */
+	public Object getObject(String key,String keyAppend) {
+		String keyName = "KEY:# " + key + (keyAppend == null ? "" : ("," + keyAppend)) + " #";
+		return redisDao.getObject(keyName);
+	}
+	public Object getObject(String key) {
+		return redisDao.getObject(key);
+	}
 }
