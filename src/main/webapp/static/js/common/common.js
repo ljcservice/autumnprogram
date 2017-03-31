b8586a19046f67b2eb12b726ba4ff096c86fd0a3
silong.xing <silong.xing@HLJ-xingsilong.ebaonet.corp>
2017-03-31 08:48:32 +0800

    --

--- src/main/webapp/static/js/common/common.js
+++ src/main/webapp/static/js/common/common.js
@@ -11,12 +11,13 @@ function reset(obj){
     			$(this).val("");
     		}
     	}else if(nodeName == 'SELECT'){
+    		$(this).find("option").attr("selected",false);
     		if($(this).attr("name")=="status"||$(this).attr("name")=="STATUS"){
+    			$(this).change();
     			$(this).find("option:[value=0]").attr("selected",true);
     		}else{
     			$(this).find("option:first").attr("selected",true);
     		}
-    		$(this).change();
 			$(this).chosen('destroy');
 			$(this).chosen({allow_single_deselect:true});
     	}		
