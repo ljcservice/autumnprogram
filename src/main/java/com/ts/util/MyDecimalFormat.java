8fd262e2f3a2bdbeac7421a1e728d5335259b45d
silong.xing <silong.xing@HLJ-xingsilong.ebaonet.corp>
2017-03-31 08:41:13 +0800

    --

---
+++ src/main/java/com/ts/util/MyDecimalFormat.java
@@ -0,0 +1,14 @@
+package com.ts.util;
+
+import java.text.DecimalFormat;
+
+public class MyDecimalFormat {
+	private static DecimalFormat decimalFormat = new DecimalFormat("0.00");
+	
+	public static String format(Double b){
+		return decimalFormat.format(b);
+	}
+	public static void main(String[] args) {
+		System.out.println(format(new Double(0)));
+	}
+}
