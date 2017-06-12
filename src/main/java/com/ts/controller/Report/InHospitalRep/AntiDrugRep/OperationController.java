33e649fdba43cf38638e3bdf530b9f4646342a98
silong.xing <silong.xing@HLJ-xingsilong.ebaonet.corp>
2017-06-12 10:07:59 +0800

    ----

--- src/main/java/com/ts/controller/Report/InHospitalRep/AntiDrugRep/OperationController.java
+++ src/main/java/com/ts/controller/Report/InHospitalRep/AntiDrugRep/OperationController.java
@@ -1,5 +1,7 @@
 package com.ts.controller.Report.InHospitalRep.AntiDrugRep;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.ts.controller.base.BaseController;
 import com.ts.entity.Page;
 import com.ts.service.Report.InHospitalRep.AntiDrugRep.IOperationService;
+import com.ts.util.ObjectExcelView;
 import com.ts.util.PageData;
 
 @Controller
@@ -54,7 +57,49 @@ public class OperationController extends BaseController
         mv.setViewName("Reprot/InHospitalRep/AntiDrugRep/DRANO004");
         return mv ;
     }
-    
+    @RequestMapping(value="/DRANO004Export")
+    public ModelAndView DRANO004Export(Page page)throws Exception{
+        ModelAndView mv = this.getModelAndView();
+        PageData pd = this.getPageData();
+        try {
+        	Map<String,Object> dataMap = new HashMap<String,Object>();
+            page.setPd(pd);
+            mv.addObject("pd", pd);
+            
+            List<String> titles = new ArrayList<String>();
+			titles.add("序号");
+			titles.add("科室");
+			if("count".equals(pd.getString("findType"))){
+				titles.add("手术总例数");
+				titles.add("使用抗菌药例数");
+			}else{
+				titles.add("手术总人次");
+				titles.add("使用抗菌药人次数");
+			}
+			titles.add("比例");
+			dataMap.put("titles", titles);
+            
+            List<PageData> varList = new ArrayList<PageData>();
+				List<PageData> varOList = oper.DRANO004(pd);
+			for(int i=0;i<varOList.size();i++){
+				PageData vpd = new PageData();
+				vpd.put("var1", i+1);	//2
+				vpd.put("var2", varOList.get(i).get("dept_name"));	//4
+				vpd.put("var3", varOList.get(i).get("coun"));		//5
+				vpd.put("var4", varOList.get(i).get("anti"));
+				vpd.put("var5", ((BigDecimal)varOList.get(i).get("ratAnti")).doubleValue()*100 );
+				varList.add(vpd);
+			}
+			dataMap.put("varList", varList);
+			ObjectExcelView erv = new ObjectExcelView();
+			mv = new ModelAndView(erv,dataMap);
+        }
+        catch(Exception e)
+        {
+            logger.error(e.toString(), e);
+        }
+        return mv ;
+    }
     @RequestMapping(value="/DRANO007UI")
     public ModelAndView DRANO07UI()throws Exception{
         ModelAndView mv = this.getModelAndView();
@@ -85,7 +130,67 @@ public class OperationController extends BaseController
         mv.setViewName("Reprot/InHospitalRep/AntiDrugRep/DRANO007");
         return mv ;
     }
-    
+    @RequestMapping(value="/DRANO007Export")
+    public ModelAndView DRANO007Export(Page page)throws Exception{
+        ModelAndView mv = this.getModelAndView();
+        PageData pd = this.getPageData();
+        try {
+        	Map<String,Object> dataMap = new HashMap<String,Object>();
+            page.setPd(pd);
+            mv.addObject("pd", pd);
+            
+            List<String> titles = new ArrayList<String>();
+			titles.add("患者姓名");
+			titles.add("手术式");
+			titles.add("医生姓名");
+			titles.add("住院科室");
+			titles.add("手术日期");
+			titles.add("原切口类型");
+			titles.add("现切口类型");
+			titles.add("抗菌药");
+			titles.add("联合用药");
+			titles.add("品种");
+			titles.add("疗程");
+			titles.add("时机");
+			dataMap.put("titles", titles);
+            
+            List<PageData> varList = new ArrayList<PageData>();
+			List<PageData> varOList =  oper.DRANO007(page);
+			for(int i=0;i<varOList.size();i++){
+				PageData vpd = new PageData();
+				vpd.put("var1", varOList.get(i).get("name"));	//2
+				vpd.put("var2", varOList.get(i).get("OPERATION_DESC"));	//4
+				vpd.put("var3", varOList.get(i).get("OPERATOR"));		//5
+				vpd.put("var4", varOList.get(i).get("dept_name"));
+				vpd.put("var5", varOList.get(i).get("OPERATING_DATE"));
+				vpd.put("var6", varOList.get(i).get("WOUND_GRADE"));
+				vpd.put("var7", varOList.get(i).get("WOUND_GRADE_UPDATE"));
+				vpd.put("var8", "1".equals(varOList.get(i).getString("HAS_ANTI"))?"是":"");
+				String LH = varOList.get(i).getString("LH");
+				if("1".equals(LH)){
+					vpd.put("var9", "一联");
+				}if("2".equals(LH)){
+					vpd.put("var9", "二联");
+				}if("3".equals(LH)){
+					vpd.put("var9", "三联");
+				}if("4".equals(LH)){
+					vpd.put("var9", "多联");
+				}
+				vpd.put("var10", "1".equals(varOList.get(i).getString("PZ"))?"是":"");
+				vpd.put("var11", "1".equals(varOList.get(i).getString("IS_TREATMENT"))?"是":"");
+				vpd.put("var12", "1".equals(varOList.get(i).getString("IS_TIMING"))?"是":"");
+				varList.add(vpd);
+			}
+			dataMap.put("varList", varList);
+			ObjectExcelView erv = new ObjectExcelView();
+			mv = new ModelAndView(erv,dataMap);
+        }
+        catch(Exception e)
+        {
+            logger.error(e.toString(), e);
+        }
+        return mv ;
+    }
     @RequestMapping(value="DRANO07_Ajax01")
     @ResponseBody
     public Object DRANO07_01() throws Exception{
@@ -136,6 +241,84 @@ public class OperationController extends BaseController
         return mv ;
     }
     
+    @RequestMapping(value="/DRANO008Export")
+    public ModelAndView DRANO008Export(Page page)throws Exception{
+        ModelAndView mv = this.getModelAndView();
+        PageData pd = this.getPageData();
+        try {
+        	Map<String,Object> dataMap = new HashMap<String,Object>();
+            page.setPd(pd);
+            mv.addObject("pd", pd);
+            
+            List<String> titles = new ArrayList<String>();
+			titles.add("序号");
+			titles.add("科室");
+			titles.add("手术总例数");
+			titles.add("抗菌药使用率");
+			titles.add("时机合理率");
+			titles.add("疗程合理率");
+			titles.add("品种合理率");
+			titles.add("一联使用率");
+			titles.add("二联使用率");
+			titles.add("三联使用率");
+			titles.add("多联使用率");
+			dataMap.put("titles", titles);
+            
+            List<PageData> varList = new ArrayList<PageData>();
+			List<PageData> varOList = oper.DRANO008(pd);
+			for(int i=0;i<varOList.size();i++){
+				PageData vpd = new PageData();
+				vpd.put("var1", i+1);
+				vpd.put("var2", varOList.get(i).get("dept_name")); 
+				vpd.put("var3", varOList.get(i).get("patsum"));	 
+				vpd.put("var4", ((BigDecimal)varOList.get(i).get("hasantisum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("ranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var5", ((BigDecimal)varOList.get(i).get("timingsum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("timingranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var6", ((BigDecimal)varOList.get(i).get("timingsum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("timingranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var7", ((BigDecimal)varOList.get(i).get("treatsum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("treatranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var8", ((BigDecimal)varOList.get(i).get("pzsum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("pzranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var9", ((BigDecimal)varOList.get(i).get("lh1sum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("lh1ranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var10",((BigDecimal)varOList.get(i).get("lh2sum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("lh2ranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var11",((BigDecimal)varOList.get(i).get("lh3sum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("lh3ranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var12",((BigDecimal)varOList.get(i).get("lh4sum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("lh4ranking")).doubleValue()*100 +"%"
+						);
+				varList.add(vpd);
+			}
+			dataMap.put("varList", varList);
+			ObjectExcelView erv = new ObjectExcelView();
+			mv = new ModelAndView(erv,dataMap);
+        }
+        catch(Exception e)
+        {
+            logger.error(e.toString(), e);
+        }
+        return mv ;
+    }
     @RequestMapping(value="/DRANO009UI")
     public ModelAndView DRANO09UI()throws Exception{
         ModelAndView mv = this.getModelAndView();
@@ -163,6 +346,82 @@ public class OperationController extends BaseController
             logger.error(e.toString(), e);
         }
         mv.setViewName("Reprot/InHospitalRep/AntiDrugRep/DRANO009");
+        return mv ;
+    }
+    @RequestMapping(value="/DRANO009Export")
+    public ModelAndView DRANO009Export(Page page)throws Exception{
+        ModelAndView mv = this.getModelAndView();
+        PageData pd = this.getPageData();
+        try {
+        	Map<String,Object> dataMap = new HashMap<String,Object>();
+            page.setPd(pd);
+            mv.addObject("pd", pd);
+            
+            List<String> titles = new ArrayList<String>();
+			titles.add("序号");
+			titles.add("医生");
+			titles.add("科室");
+			titles.add("手术总例数");
+			titles.add("抗菌药使用率");
+			titles.add("时机合理率");
+			titles.add("疗程合理率");
+			titles.add("品种合理率");
+			titles.add("一联使用率");
+			titles.add("二联使用率");
+			titles.add("三联使用率");
+			titles.add("多联使用率");
+			dataMap.put("titles", titles);
+            
+            List<PageData> varList = new ArrayList<PageData>();
+			List<PageData> varOList = oper.DRANO009(pd);
+			for(int i=0;i<varOList.size();i++){
+				PageData vpd = new PageData();
+				vpd.put("var1", i+1);	//2
+				vpd.put("var2", varOList.get(i).get("operator"));	//4
+				vpd.put("var3", varOList.get(i).get("dept_name"));		//5
+				vpd.put("var4", varOList.get(i).get("patsum"));
+				vpd.put("var5", ((BigDecimal)varOList.get(i).get("hasantisum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("ranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var6", ((BigDecimal)varOList.get(i).get("timingsum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("timingranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var7", ((BigDecimal)varOList.get(i).get("treatsum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("treatranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var8", ((BigDecimal)varOList.get(i).get("pzsum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("pzranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var9", ((BigDecimal)varOList.get(i).get("lh1sum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("lh1ranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var10",((BigDecimal)varOList.get(i).get("lh2sum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("lh2ranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var11",((BigDecimal)varOList.get(i).get("lh3sum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("lh3ranking")).doubleValue()*100 +"%"
+						);
+				vpd.put("var12",((BigDecimal)varOList.get(i).get("lh4sum")).doubleValue()
+						+"/"+((BigDecimal)varOList.get(i).get("patsum")).doubleValue()+"="
+						+ ((BigDecimal)varOList.get(i).get("lh4ranking")).doubleValue()*100 +"%"
+						);
+				varList.add(vpd);
+			}
+			dataMap.put("varList", varList);
+			ObjectExcelView erv = new ObjectExcelView();
+			mv = new ModelAndView(erv,dataMap);
+        }
+        catch(Exception e)
+        {
+            logger.error(e.toString(), e);
+        }
         return mv ;
     }
 }
